94cdb0b9186ff13e0580c6891519cd8fd9e52304
Jefferson O <jortizr@ut.edu.co>
2023-05-07 18:50:05 -0500

    desarrollo del frontend

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,3 +1,6 @@
+//importamos el modulo nativo del api de node
+import os from 'os'
 window.addEventListener('load', () => {
   //document.getElementById('mensaje').innerHTML = 'Este es un mensaje insertado por JS'
+  console.log(os.cpus())
 })
