68afbefb8e551b73ca321a82637abfdf7e399e93
Jefferson O <jortizr@ut.edu.co>
2023-05-10 22:26:44 -0500

    funciones de search, seleccion y visualizacion

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,57 +1,74 @@
 //url para convertir el url en un array
-import url from 'url'
-import path from 'path'
+import url from "url";
+import path from "path";
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   //document.getElementById('mensaje').innerHTML = 'Este es un mensaje insertado por JS'
-  addImageEvents()
-  searchImageEvent()
-})
+  addImageEvents();
+  searchImageEvent();
+});
 
-function addImageEvents (){
+function addImageEvents() {
   //obtiene todos los elementos a alterar
-  const thumbs = document.querySelectorAll('li.list-group-item')
+  const thumbs = document.querySelectorAll("li.list-group-item");
   //recorrer agregando un evento
-  for (var i = 0, length1 = thumbs.length; i < length1; i++){
-    thumbs[i].addEventListener('click', function () {
-      changeImage(this)
-    })
+  for (var i = 0, length1 = thumbs.length; i < length1; i++) {
+    thumbs[i].addEventListener("click", function () {
+      changeImage(this);
+    });
   }
-
 }
 
 //funcion que cambia las imagenes y envia el id del elemento seleccionado
 function changeImage(node) {
-  document.querySelector('li.selected').classList.remove('selected')
+  document.querySelector("li.selected").classList.remove("selected");
   //ahora le pasamos el selected al elemento seleccionado
-  node.classList.add('selected')
+  node.classList.add("selected");
   //cambiamos la imagen del panel visor
-  const image = document.getElementById('image-displayed')
-    image.src = node.querySelector('img').src
-    image.dataset.original = image.src
+  const image = document.getElementById("image-displayed");
+  image.src = node.querySelector("img").src;
+  image.dataset.original = image.src;
 }
 
 //creamos la funcion que escucha el evento keyup de input de search
-function searchImageEvent() { 
+function searchImageEvent() {
   //obtenemos el input
-  const searchBox = document.getElementById('search-box')
+  const searchBox = document.getElementById("search-box");
   //escuchamos el evento keyup
-  searchBox.addEventListener('keyup', function() { 
+  searchBox.addEventListener("keyup", function () {
+    //creamos una expresion regular para validar si lo del input cumple
+    //con la condicion //this.value valor de la caja de texto
+    const regex = new RegExp(this.value.toLowerCase(), "gi");
+    console.log(this.value.length);
     //ejecutamos el evento si esto es mayor que 0
     if (this.value.length > 0) {
       //si las imagenes de la vista cumplen la condicion
       //obtener todas las imagenes de la lista
-      const thumbs = document.querySelectorAll('li.list-group-item img')
+      const thumbs = document.querySelectorAll("li.list-group-item img");
       //el for consigue todas las imagenes
-      for (let i = 0, length1 = thumbs.length; i < length1; i++){
-        const fileUrl = url.parse(thumbs[i].src) //obtiene es una url pero se convier
+      for (let i = 0, length1 = thumbs.length; i < length1; i++) {
+        const fileUrl = url.parse(thumbs[i].src); //obtiene es una url pero se convier
         //en un array
         //ahora conseguimos el nombre del archivo
-        const fileName = path.basename(fileUrl.pathname) //importamos path
-        console.log(fileName)
+        const fileName = path.basename(fileUrl.pathname); //importamos path
+        //se valida si el nombre cumple con el de los archivo
+        if (fileName.match(regex)) {
+          //si es asi lo hacemos visible
+          thumbs[i].parentNode.classList.remove("hidden");
+        } else {
+          //sino agregamos la clase hidden para hacerla invisible
+          thumbs[i].parentNode.classList.add("hidden");
+        }
       }
+
+      selectFirstImage();
     }
-    
-  })
-  
+  });
+}
+
+//funcion que selecciona la primera imagen actual y seleccionada
+function selectFirstImage() {
+  //obtenemos todas las imagenes
+  const thumbs = document.querySelector("li.list-group-item:not(.hidden)");
+  changeImage(thumbs);
 }
